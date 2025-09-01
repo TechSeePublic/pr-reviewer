fa9c09933c1bb8cc49b2c77ec9d241b127b59520
amitwa1 <153432188+amitwa1@users.noreply.github.com>
2025-09-01 12:11:20 +0300

    chore: add built files for release v1.0.33

---
+++ dist/logger.d.ts
@@ -0,0 +1,10 @@
+/**
+ * Logging utility
+ */
+export declare const logger: {
+    info: (message: string) => void;
+    warn: (message: string, error?: unknown) => void;
+    error: (message: string, error?: unknown) => void;
+    debug: (message: string) => void;
+};
+//# sourceMappingURL=logger.d.ts.map
